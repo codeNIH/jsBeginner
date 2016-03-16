f343d238055bd6f83bec0beea4ff3ef36fc1df8e
codeNIH <codenih@gmail.com>
2016-03-16 12:12:51 -0400

    Create ex06.js

---
+++ ex06.js
@@ -0,0 +1,5 @@
+Write a JavaScript program to convert temperatures to and from celsius, fahrenheit.
+[ Formula : c/5 = (f-32)/9 [ where c = temperature in celsius and f = temperature in fahrenheit ] 
+Expected Output : 
+60°C is 140 °F
+45°F is 7.222222222222222°C 
